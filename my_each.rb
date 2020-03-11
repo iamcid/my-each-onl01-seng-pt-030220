71fb1d2dc260ce0f0d308b2499728a05528cea26
Christopher Cid <iamcid17@gmail.com>
2020-03-11 23:55:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,14 @@
-def my_each(num)
+def my_each(array)
   if block_given?
-   i = 0
-   while i < array.length
-     yield([i])
-     i = i + 1
-   end
-  
-   array
+    i = 0
+    
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    
+    array
   else
-    return 
-  
+    return "Hey! No block was given!"
+  end
 end
